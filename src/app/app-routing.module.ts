b3dc9248cbc306fab8ddb7066c64c10156b5a05b
Luciano Portales <lportales655@gmail.com>
2021-10-18 23:23:26 -0300

    tarea 3 - Luciano Portales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerServiceComponent } from './pages/customer-service/customer-service.component';
+import { HomeComponent } from './pages/home/home.component'
+import { NewsComponent } from './pages/news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'noticias', component: NewsComponent},
+  {path: 'servicio-al-cliente', component: CustomerServiceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewsComponent } from './pages/news/news.component';
+import { CustomerServiceComponent } from './pages/customer-service/customer-service.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CardComponent,
+    HomeComponent,
+    NewsComponent,
+    CustomerServiceComponent,
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
