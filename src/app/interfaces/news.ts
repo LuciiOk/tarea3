ba7b7df4166117d1dde7c6a74dcd79553c5f9328
Luciano Portales <lportales655@gmail.com>
2021-10-20 00:44:16 -0300

    tarea 3 - luciano portales

--- src/app/interfaces/news.ts
+++ src/app/interfaces/news.ts
@@ -2,12 +2,50 @@ export interface News {
     titulo:string,
     descripcion:string,
     img:string,
+    categoria:string
 }
 
 export let noticias:News[] = [
     {
         titulo: 'El juego de Calamar se convierte en la serie mas vista de netflix',
         descripcion: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
-        img: ''
+        img: 'https://i.blogs.es/100784/1177246/1366_2000.jpeg',
+        categoria: 'entretenimiento'
+    },
+    {
+        titulo: 'El juego de Calamar se convierte en la serie mas vista de netflix',
+        descripcion: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+        img: 'https://i.blogs.es/100784/1177246/1366_2000.jpeg',
+        categoria: 'salud'
+    },
+    {
+        titulo: 'El juego de Calamar se convierte en la serie mas vista de netflix',
+        descripcion: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+        img: 'https://i.blogs.es/100784/1177246/1366_2000.jpeg',
+        categoria: 'deporte'
+    },
+    {
+        titulo: 'El juego de Calamar se convierte en la serie mas vista de netflix',
+        descripcion: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+        img: 'https://i.blogs.es/100784/1177246/1366_2000.jpeg',
+        categoria: ''
+    },
+    {
+        titulo: 'El juego de Calamar se convierte en la serie mas vista de netflix',
+        descripcion: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+        img: 'https://i.blogs.es/100784/1177246/1366_2000.jpeg',
+        categoria: ''
+    },
+    {
+        titulo: 'El juego de Calamar se convierte en la serie mas vista de netflix',
+        descripcion: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+        img: 'https://i.blogs.es/100784/1177246/1366_2000.jpeg',
+        categoria: ''
+    },
+    {
+        titulo: 'El juego de Calamar se convierte en la serie mas vista de netflix',
+        descripcion: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+        img: 'https://i.blogs.es/100784/1177246/1366_2000.jpeg',
+        categoria: ''
     },
 ]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import {News, noticias} from '../../interfaces/news'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+ 
+  noticias:News[] = noticias;
+  noticiasDifCat:News[] = [];
+  noticiasCat:News[] = [];
 
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
+    this.noticiasDifCat = noticias.filter((noticia:News)=> 
+      noticia.categoria != 'entretenimiento' && noticia.categoria != 'economia' &&
+      noticia.categoria != 'deporte' && noticia.categoria != 'salud' 
+      && noticia.categoria != 'tecnologia' && noticia.categoria != 'mundo');
+
+    this.noticiasCat = noticias.filter((noticia:News)=> 
+      noticia.categoria == 'entretenimiento' || noticia.categoria == 'economia' ||
+      noticia.categoria == 'deporte' || noticia.categoria == 'salud' 
+      || noticia.categoria == 'tecnologia' || noticia.categoria == 'mundo');
   }
 
 }
